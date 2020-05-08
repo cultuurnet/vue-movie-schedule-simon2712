05bc1d06b99a9efcd5256ce3b0d966aee10be4b6
Bram Pauwelyn <brampauwelyn@users.noreply.github.com>
2020-05-08 10:41:08 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img alt="publiq logo" class="logo" src="./assets/logo.png">
+    <h1>Movie Schedule</h1>
+    <p>Add your component here</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 50px;
+}
+.logo{
+  width: 100px;
+}
+</style>
